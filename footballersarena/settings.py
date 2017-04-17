30847948fb949536625eed2472aa699273436438
harshit garg <harshit@esthenos.com>
2017-04-17 13:42:33 +0530

    change the static file location

--- footballersarena/settings.py
+++ footballersarena/settings.py
@@ -125,6 +125,6 @@ MEDIA = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 # Add these new lines
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/footballers'),
 )
 
