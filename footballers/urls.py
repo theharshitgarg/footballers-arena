897c77698d8f89b60838b865a7dde89af1f6bd0f
harshit garg <harshit@esthenos.com>
2017-04-19 08:38:19 +0530

    Add partial list feature to see partial list of players

--- footballers/urls.py
+++ footballers/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     url(r'^$', login_required(views.PlayersListView.as_view()),  name='players'),
+    url(r'^partial-list$', views.PlayersListPartialView.as_view(),  name='partial-players-list'),
     # url(r'^$', views.PlayersListView.as_view(), name='player_detail'),
     url(r'^(?P<pk>[-\w]+)/$', views.player_detail_view, name='player-detail')
 ]
--- footballers/views.py
+++ footballers/views.py
@@ -20,6 +20,18 @@ class PlayersListView(generic.ListView):
         return FootballPlayer.objects.all()
 
 
+class PlayersListPartialView(generic.ListView):
+
+    template_name = 'footballers/footballers_list.html'
+    context_object_name = 'footballers_list'
+    paginate_by = 10
+
+    def get_queryset(self):
+        """Return all the objects."""
+
+        return FootballPlayer.objects.all()[:15]
+
+
 def player_detail_view(request, pk):
     player = get_object_or_404(FootballPlayer, pk=pk)
     serialized_player = player.json_equivalent
